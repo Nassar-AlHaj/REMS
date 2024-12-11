b44e8c1882c6d3a99575c68120dfa6aaee1483c4
Nassar Al-Haj <152079213+Nassar-AlHaj@users.noreply.github.com>
2024-12-11 17:51:09 +0200

    Initial commit for JavaFX real estate management project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.rems.realestatemanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.rems.realestatemanagement to javafx.fxml;
+    exports com.rems.realestatemanagement;
+}

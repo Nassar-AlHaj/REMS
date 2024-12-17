ffa6c9abd006e41cfe1a621c0dc752ad5a1c5949
nadarinnoo <151661333+nadarinnoo@users.noreply.github.com>
2024-12-17 21:07:22 +0200

    frontend

--- src/main/java/com/rems/realestatemanagement/HelloApplication.java
+++ src/main/java/com/rems/realestatemanagement/HelloApplication.java
@@ -1,5 +1,4 @@
 package com.rems.realestatemanagement;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,8 +9,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/rems/realestatemanagement/Fxml/login.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();

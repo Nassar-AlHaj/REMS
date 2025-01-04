730a694b26e258e833df8105f4c0e6dd2e9f0fc4
Nassar Al-Haj <152079213+Nassar-AlHaj@users.noreply.github.com>
2025-01-04 22:10:37 +0200

    Add categorization to client

--- src/main/java/com/rems/realestatemanagement/models/Client.java
+++ src/main/java/com/rems/realestatemanagement/models/Client.java
@@ -25,23 +25,38 @@ public class Client {
     @Column(name = "address", nullable = false)
     private String address;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "categorization", nullable = false)
+    private ClientCategory categorization;
+
     @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Interaction> interactions;
+
+    public enum ClientCategory {
+        VIP, Residentials, Commercial_Clients, Brokers, Investors
+    }
+
+
 
 
     public Client() {}
 
-    public Client(String fname, String lname, String phone, String email, String address) {
+    public Client(String fname, String lname, String phone, String email, String address, ClientCategory categorization) {
         this.fname = fname;
         this.lname = lname;
         this.phone = phone;
         this.email = email;
         this.address = address;
+        this.categorization = categorization;
     }
-
     public int getId() {
         return id;
     }
+
+
+
+
+
 
 
 
@@ -84,4 +99,15 @@ public class Client {
     public void setAddress(String address) {
         this.address = address;
     }
+
+
+    public ClientCategory getCategorization() {
+        return categorization;
+    }
+
+    public void setCategorization(ClientCategory categorization) {
+        this.categorization = categorization;
+    }
 }
+
+

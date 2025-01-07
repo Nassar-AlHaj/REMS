bafb2373f949da7fa30bc7c6c12bca90d5473a30
Mahmoud-Jawabreh <m.jawabreh720@gmail.com>
2025-01-07 16:12:23 +0200

    red stars added and routing onto CreateOffer page is done

--- src/main/java/com/rems/realestatemanagement/Controller/CreateOfferController.java
+++ src/main/java/com/rems/realestatemanagement/Controller/CreateOfferController.java
@@ -1,5 +1,8 @@
 package com.rems.realestatemanagement.Controller;
 
+import com.rems.realestatemanagement.models.OffersDAOimp;
+import com.rems.realestatemanagement.models.interfaces.OffersDAO;
+import com.rems.realestatemanagement.models.services.Offers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -9,7 +12,7 @@ import javafx.stage.Stage;
 public class CreateOfferController {
 
     @FXML
-    private TextField clientName, propertyId, amountField;
+    private TextField clientNameField, propertyId, amountField;
 
     @FXML
     private ComboBox<String> typeComboBox;
@@ -17,20 +20,44 @@ public class CreateOfferController {
     @FXML
     private Button cancelButton, createButton;
 
+    private OffersDAO offersDAO;
+
     @FXML
     public void initialize() {
+        System.out.println("Initializing CreateOfferController...");
         typeComboBox.getItems().addAll("Rent", "Purchase");
+        typeComboBox.setValue("Purchase");
+
+        offersDAO = new OffersDAOimp();
 
         cancelButton.setOnAction(event -> closeModal());
-        createButton.setOnAction(event -> saveOffer());
+        createButton.setOnAction(event -> CreateOffer());
     }
 
     private void closeModal() {
         ((Stage) cancelButton.getScene().getWindow()).close();
     }
 
-    private void saveOffer() {
+    private void CreateOffer() {
+        // Create a new Offers object using data from the UI
+        String clientNameText = clientNameField.getText();
+        String propertyIdText = propertyId.getText();
+        System.out.println("Property ID: " + propertyIdText);
+        double amount = Double.parseDouble(amountField.getText());
+        String type = typeComboBox.getValue();
+
+        // Create an offer object
+        Offers newOffer = new Offers(clientNameText, propertyIdText, type, amount);
+
+        // Use the DAO to save the offer
+        offersDAO.CreateOffer(newOffer);
+
         System.out.println("Offer Created!");
+
+        // Close the modal after saving
         closeModal();
     }
+
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,9 +8,17 @@ module com.rems.realestatemanagement {
     requires org.kordamp.bootstrapfx.core;
     requires com.almasb.fxgl.all;
     requires java.desktop;
+    requires org.hibernate.orm.core;
+    requires java.persistence;
+    requires mysql.connector.java;
+    requires java.sql;
+    requires java.naming;
 
+    opens com.rems.realestatemanagement to org.hibernate.orm.core;
     opens com.rems.realestatemanagement.Controller to javafx.fxml;
+    opens com.rems.realestatemanagement.models.services to org.hibernate.orm.core;
     exports com.rems.realestatemanagement;
     exports com.rems.realestatemanagement.Controller;
+    exports com.rems.realestatemanagement.models.services to org.hibernate.orm.core;
 }
 
